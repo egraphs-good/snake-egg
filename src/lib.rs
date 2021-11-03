0975c42e498addc1405b73f636c1f26373627ad0
Max Willsey <me@mwillsey.com>
2021-11-03 13:56:09 -0700

    Get rewrites working

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,127 @@
-use std::hash::Hash;
+use std::{array::IntoIter, collections::HashMap, hash::Hash, time::Duration};
 
-use pyo3::{basic::CompareOp, prelude::*, types::PyTuple, PyObjectProtocol};
+use pyo3::{
+    basic::CompareOp,
+    prelude::*,
+    types::{PyList, PyString, PyTuple},
+    AsPyPointer, PyNativeType, PyObjectProtocol, ToPyObject,
+};
 
-// #[pyclass(subclass)]
-// #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-// struct ENode {
-//     operator: egg::Symbol,
-//     children: Vec<egg::Id>,
-// }
+macro_rules! impl_py_object {
+    ($t:ty) => {
+        #[pyproto]
+        impl PyObjectProtocol for $t {
+            fn __str__(&self) -> String {
+                self.0.to_string()
+            }
+
+            fn __repr__(&self) -> String {
+                format!(concat!(stringify!($t), "({})"), self.0)
+            }
+
+            fn __richcmp__(&self, other: Self, op: CompareOp) -> bool {
+                match op {
+                    CompareOp::Lt => self.0 < other.0,
+                    CompareOp::Le => self.0 <= other.0,
+                    CompareOp::Eq => self.0 == other.0,
+                    CompareOp::Ne => self.0 != other.0,
+                    CompareOp::Gt => self.0 > other.0,
+                    CompareOp::Ge => self.0 >= other.0,
+                }
+            }
+        }
+    };
+}
 
 #[pyclass]
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 struct Id(egg::Id);
 
-#[pyproto]
-impl PyObjectProtocol for Id {
-    fn __str__(&self) -> String {
-        self.0.to_string()
-    }
-    fn __repr__(&self) -> String {
-        self.0.to_string()
-    }
+impl_py_object!(Id);
 
-    fn __richcmp__(&self, other: Self, op: CompareOp) -> bool {
-        match op {
-            CompareOp::Lt => self.0 < other.0,
-            CompareOp::Le => self.0 <= other.0,
-            CompareOp::Eq => self.0 == other.0,
-            CompareOp::Ne => self.0 != other.0,
-            CompareOp::Gt => self.0 > other.0,
-            CompareOp::Ge => self.0 >= other.0,
-        }
+#[pyclass]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+struct Var(egg::Var);
+
+impl_py_object!(Var);
+
+impl Var {
+    fn from_str(str: &str) -> Self {
+        let v = format!("?{}", str);
+        Var(v.parse().unwrap())
+    }
+}
+
+#[pymethods]
+impl Var {
+    #[new]
+    fn new(str: &PyString) -> Self {
+        Self::from_str(str.to_string_lossy().as_ref())
+    }
+}
+
+#[pyclass]
+struct Pattern {
+    pattern: egg::Pattern<PyLang>,
+}
+
+#[pymethods]
+impl Pattern {
+    #[new]
+    fn new(tree: &PyAny) -> Self {
+        let mut ast = egg::PatternAst::default();
+        build_pattern(&mut ast, tree);
+        let pattern = egg::Pattern::from(ast);
+        Self { pattern }
+    }
+}
+
+fn build_pattern(ast: &mut egg::PatternAst<PyLang>, tree: &PyAny) -> egg::Id {
+    if let Ok(id) = tree.extract::<Id>() {
+        panic!("Ids are unsupported in patterns: {}", id.0)
+    } else if let Ok(var) = tree.extract::<Var>() {
+        ast.add(egg::ENodeOrVar::Var(var.0))
+    } else if let Ok(tuple) = tree.downcast::<PyTuple>() {
+        let ty = tree.get_type_ptr() as usize;
+        let ids: Vec<egg::Id> = tuple
+            .iter()
+            .map(|child| build_pattern(ast, child))
+            .collect();
+        ast.add(egg::ENodeOrVar::ENode(PyLang::Node(ty, ids)))
+    } else if let Ok(n) = tree.extract::<i64>() {
+        ast.add(egg::ENodeOrVar::ENode(PyLang::Int(n)))
+    } else {
+        let repr = tree.repr().expect("failed to repr");
+        panic!("Cannot convert to pattern: {}", repr)
+    }
+}
+
+#[pyclass]
+struct Rewrite {
+    rewrite: egg::Rewrite<PyLang, ()>,
+}
+
+#[pymethods]
+impl Rewrite {
+    #[new]
+    fn new(from: &PyAny, to: &PyAny) -> Self {
+        let searcher = Pattern::new(from).pattern;
+        let applier = Pattern::new(to).pattern;
+        let rewrite =
+            egg::Rewrite::new("name", searcher, applier).expect("Failed to create rewrite");
+        Rewrite { rewrite }
     }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-enum ENode {
+enum PyLang {
     Int(i64),
     Node(usize, Vec<egg::Id>),
 }
 
-impl egg::Language for ENode {
+impl egg::Language for PyLang {
     fn matches(&self, other: &Self) -> bool {
-        use ENode::*;
+        use PyLang::*;
         match (self, other) {
             (Node(op1, args1), Node(op2, args2)) => op1 == op2 && args1.len() == args2.len(),
             (Int(a), Int(b)) => a == b,
@@ -52,14 +131,14 @@ impl egg::Language for ENode {
 
     fn children(&self) -> &[egg::Id] {
         match self {
-            ENode::Node(_, args) => args,
+            PyLang::Node(_, args) => args,
             _ => &[],
         }
     }
 
     fn children_mut(&mut self) -> &mut [egg::Id] {
         match self {
-            ENode::Node(_, args) => args,
+            PyLang::Node(_, args) => args,
             _ => &mut [],
         }
     }
@@ -68,8 +147,10 @@ impl egg::Language for ENode {
 #[pyclass]
 #[derive(Default)]
 pub struct EGraph {
-    egraph: egg::EGraph<ENode, ()>,
+    egraph: egg::EGraph<PyLang, ()>,
 }
+
+type Runner = egg::Runner<PyLang, (), ()>;
 
 #[pymethods]
 impl EGraph {
@@ -95,8 +176,45 @@ impl EGraph {
         did_something
     }
 
+    #[args(exprs = "*")]
+    fn equiv(&mut self, exprs: &PyTuple) -> bool {
+        assert!(exprs.len() > 1);
+        let mut exprs = exprs.iter();
+        let id = self.add(exprs.next().unwrap()).0;
+        let mut all_equiv = true;
+        for expr in exprs {
+            let added = self.add(expr);
+            all_equiv &= added.0 == id
+        }
+        all_equiv
+    }
+
     fn rebuild(&mut self) -> usize {
         self.egraph.rebuild()
+    }
+
+    #[args(iters = "10", time_limit = "10.0", node_limit = "100_000")]
+    fn run(
+        &mut self,
+        rewrites: &PyList,
+        iter_limit: usize,
+        time_limit: f64,
+        node_limit: usize,
+    ) -> PyResult<()> {
+        let refs = rewrites
+            .iter()
+            .map(FromPyObject::extract)
+            .collect::<PyResult<Vec<PyRef<Rewrite>>>>()?;
+        let egraph = std::mem::take(&mut self.egraph);
+        let runner = Runner::new(())
+            .with_iter_limit(iter_limit)
+            .with_node_limit(node_limit)
+            .with_time_limit(Duration::from_secs_f64(time_limit))
+            .with_egraph(egraph)
+            .run(refs.iter().map(|r| &r.rewrite));
+
+        self.egraph = runner.egraph;
+        Ok(())
     }
 }
 
@@ -107,15 +225,29 @@ impl EGraph {
         } else if let Ok(tuple) = expr.downcast::<PyTuple>() {
             let ty = expr.get_type_ptr() as usize;
             let ids: Vec<egg::Id> = tuple.iter().map(|child| self.add_rec(child)).collect();
-            self.egraph.add(ENode::Node(ty, ids))
+            self.egraph.add(PyLang::Node(ty, ids))
         } else if let Ok(n) = expr.extract::<i64>() {
-            self.egraph.add(ENode::Int(n))
+            self.egraph.add(PyLang::Int(n))
         // } else if let Ok(s) = expr.extract::<&str>() {
         //     self.egraph.add(ENode::Symbol(s.into()))
         } else {
             let repr = expr.repr().expect("failed to repr");
             panic!("Cannot add {}", repr)
         }
+    }
+}
+
+fn singleton_or_tuple<T, TS>(py: Python<'_>, elems: TS) -> PyObject
+where
+    T: IntoPy<PyObject>,
+    TS: IntoIterator<Item = T>,
+    TS::IntoIter: ExactSizeIterator<Item = T>,
+{
+    let mut elems = elems.into_iter();
+    if elems.len() == 1 {
+        elems.next().unwrap().into_py(py)
+    } else {
+        PyTuple::new(py, elems.map(|x| x.into_py(py))).into_py(py)
     }
 }
 
@@ -126,5 +258,15 @@ impl EGraph {
 fn snake_egg(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<EGraph>()?;
     m.add_class::<Id>()?;
+    m.add_class::<Var>()?;
+    m.add_class::<Pattern>()?;
+    m.add_class::<Rewrite>()?;
+
+    #[pyfn(m)]
+    fn vars(py: Python<'_>, vars: &PyString) -> PyObject {
+        let s = vars.to_string_lossy();
+        let strs: Vec<&str> = s.split_whitespace().collect();
+        singleton_or_tuple(py, strs.iter().map(|s| Var::from_str(s)))
+    }
     Ok(())
 }
--- test.py
+++ test.py
@@ -1,22 +1,48 @@
+from typing import Any, Tuple
 import unittest
-import snake_egg
+
 
 from collections import namedtuple
+from snake_egg import EGraph, Rewrite, Var, vars
 
 Add = namedtuple('Add', 'x y')
 Mul = namedtuple('Mul', 'x y')
 
+x, y = vars('x y')
+
+rules = [
+    Rewrite(Add(x, x), to=Mul(x, 2))
+]
+
+
+egraph = EGraph()
+
+add = egraph.add(Add(1, 1))
+mul = egraph.add(Mul(1, 2))
+
+egraph.run(rules, iter_limit=1)
+assert egraph.equiv(add, mul)
+
 
 class TestEGraph(unittest.TestCase):
 
+    def test_vars(self):
+        self.assertEqual(x, Var('x'))
+        self.assertEqual(str(x), '?x')
+        self.assertEqual(repr(x), 'Var(?x)')
+
     def test_simple(self):
-        egraph = snake_egg.EGraph()
-        a123 = egraph.add(Add(1, Add(2, 3)))
-        egraph.add(Add(a123, 4))
-        print(str(a123))
+        egraph = EGraph()
+
+        add = egraph.add(Add(1, 1))
+        mul = egraph.add(Mul(1, 2))
+
+        egraph.run(rules, iter_limit=1)
+
+        self.assertTrue(egraph.equiv(add, mul))
 
         self.assertEqual(egraph.add(Add(7, 8)), egraph.add(Add(7, 8)))
-        self.assertTrue(egraph.union(2, Add(1, 1)))
+        self.assertTrue(egraph.union(2, Add(1, 1), Add(2, 0)))
 
 
 if __name__ == '__main__':

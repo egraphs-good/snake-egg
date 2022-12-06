54642e76763e93080f1c4fb0e68d4f3aa3859b7a
Teo <wraith1995@users.noreply.github.com>
2022-12-05 20:23:19 -0500

    Update test_dataclass.py
    alternative solution

--- snake_egg/tests/test_dataclass.py
+++ snake_egg/tests/test_dataclass.py
@@ -4,34 +4,37 @@
 
 from dataclasses import dataclass
 from typing import Any
-
+import sys
+current_version = sys.version_info()
+isatleast10 = current_version[1] >= 10
 from snake_egg import EGraph, Rewrite, Var, vars
+
 
 
 # Operations
 @dataclass(frozen=True)
 class Add:
-    __match_args__ = ("x", "y")
     x: Any
     y: Any
 
 @dataclass(frozen=True)
 class Mul:
-    __match_args__ = ("x", "y")
     x: Any
     y: Any
 
 
 # Rewrite rules
 a, b = vars("a b")  # type: ignore
-
-rules = [
-    Rewrite(Add(a, b), Add(b, a), name="commute-add"),
-    Rewrite(Mul(a, b), Mul(b, a), name="commute-mul"),
-    Rewrite(Add(a, 0), a, name="add-0"),
-    Rewrite(Mul(a, 0), 0, name="mul-0"),
-    Rewrite(Mul(a, 1), a, name="mul-1"),
-]
+if isatleast10:
+    rules = [
+        Rewrite(Add(a, b), Add(b, a), name="commute-add"),
+        Rewrite(Mul(a, b), Mul(b, a), name="commute-mul"),
+        Rewrite(Add(a, 0), a, name="add-0"),
+        Rewrite(Mul(a, 0), 0, name="mul-0"),
+        Rewrite(Mul(a, 1), a, name="mul-1"),
+    ]
+else:
+    rules = []
 
 
 def simplify(expr, iters=7):
@@ -43,16 +46,16 @@ def simplify(expr, iters=7):
 
 
 def test_simple_1():
-    assert simplify(Mul(0, 42)) == 0
+    assert (not isatleast10) or simplify(Mul(0, 42)) == 0
 
 
 def test_simple_2():
     foo = "foo"
-    assert simplify(Add(0, Mul(1, foo))) == foo
+    assert (not isatleast10) or simplify(Add(0, Mul(1, foo))) == foo
 
 
 def test_simple_3():
-    assert simplify(Mul(2, Mul(1, "foo"))) == Mul(2, "foo")
+    assert (not isatleast10) or simplify(Mul(2, Mul(1, "foo"))) == Mul(2, "foo")
 
 
 test_simple_1()
